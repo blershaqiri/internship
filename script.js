611aab6ba33107d165285f0798d78c3dc11922a6
Blerim Shaqiri <blerimshakiri@gmail.com>
2022-09-05 14:20:01 +0200

    completed the first section BASICS

---
+++ script.js
@@ -0,0 +1,69 @@
+// let blerim = "intern";
+// console.log(blerim);
+
+/*let firstName = 'Blerim';
+let lastName = 'Shaqiri';
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//Types of values / Primitives
+
+let nameOla = 'Blerim';
+let age = 26;
+let isApproved = true;
+let firstNemojbrate = undefined;
+let selectColor = null;
+*/
+
+/*
+
+let person = {
+    name: 'blerim',
+    age: 26
+}
+
+// Changing the name of the person inside the object (Dot notation)
+person.age = 99;
+person.name = 'Scot';
+
+//Changing the name via (Bracket Notation)
+
+person['name'] = 'John Snow';
+
+
+
+console.log(person);
+
+*/
+
+/*
+let selectColors = ['red', 'blue'];
+selectColors[2] = 1;
+console.log(selectColors.length);
+*/
+
+//Learning Functions
+
+
+
+function greet(name, lastName) {
+    console.log(`Hello` + name + lastName);
+}
+greet('Blerim', ' Shaqiri');
+greet('John', ' Snow');
+
+//Function that calculates a value
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(250);
+console.log(number);
+
+console.log(square(959));
+
+//
+// 
